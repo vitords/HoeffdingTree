3a7d24f0094be2f91aae7e65389de55f00ee78b3
Vitor da Silva <vitords.42@gmail.com>
2016-03-22 04:21:54 -0300

    More docs and indentation fixes.

--- ht/conditionalsufficientstats.py
+++ ht/conditionalsufficientstats.py
@@ -1,19 +1,46 @@
 from abc import ABCMeta, abstractmethod
 
 class ConditionalSufficientStats(metaclass=ABCMeta):
-	"""docstring for ConditionalSufficientStats"""
-	def __init__(self):
-		# Tuples (class value, attribute estimator)
-		self._class_lookup = {}
+    """A class for keeping record of the sufficient statistics for an attribute."""
+    def __init__(self):
+        # Lookup by class value
+        # Dict of tuples (class value, attribute estimator)
+        self._class_lookup = {}
 
-	@abstractmethod
-	def update(self, att_val, class_val, weight):
-		pass
-	
-	@abstractmethod
-	def probability_of_att_val_conditioned_on_class(self, att_val, class_val):
-		pass
+    @abstractmethod
+    def update(self, att_val, class_val, weight):
+        """Update the statistics with the supplied attribute and class values.
 
-	@abstractmethod
-	def best_split(self, split_metric, pre_split_dist, att_name):
-		pass
+        Args:
+            att_val (float): The value of the attribute.
+            class_val (str): The value of the class.
+            weight (float): The weight of this observation.
+        """
+        pass
+    
+    @abstractmethod
+    def probability_of_att_val_conditioned_on_class(self, att_val, class_val):
+        """Return the probability of an attribute value conditioned on a class value.
+
+        Args:
+            att_val (float): The attribute value to compute the conditional probability for.
+            class_val (str): The class value.
+
+        Returns:
+            float: The probability of the attribute value being conditioned on the given class value.
+        """
+        pass
+
+    @abstractmethod
+    def best_split(self, split_metric, pre_split_dist, att_name):
+        """Return the best split.
+
+        Args:
+            split_metric (SplitMetric): The split metric to use.
+            pre_split_dist (dict): The distribution of class values before the split.
+            att_name (str): The name of the attribute being considered for splitting.
+
+        Returns:
+            SplitCandidate: The best split for the attribute.
+        """
+        pass
